2a6d15498e0b68c45d66b745855ca16efcbeb741
Alex <kpuk77@bk.ru>
2020-12-15 02:09:01 +0800

    Add project files.

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+//                               Задание 2.
+//  Разработать класс Message, содержащий следующие статические методы для обработки текста:
+//      а) Вывести только те слова сообщения, которые содержат не более n букв.
+//      б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+//      в) Найти самое длинное слово сообщения.
+//      г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+//  Продемонстрируйте работу программы на текстовом файле с вашей программой.
+
+//  Ступин А.А.
+
+namespace Exercise_2
+{
+    static class Message
+    {
+        public static string NoLonger(string msg, int length)
+        {
+            string str = DeletePunctuation(msg);
+            string[] s = str.Split(' ');
+            string selected = String.Empty;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i].Length < length)
+                    selected += $"{String.Join(" ", s[i])} ";
+            }
+            selected.TrimEnd(' ');
+
+            return selected;
+        }
+
+        public static string Delete(string msg, char ch)
+        {
+            string[] s = msg.Split(' ');
+            string[] s2 = msg.Split(',', '.', '!', '?', ' ');
+            s2 = DeleteEmptyStrings(s2);
+
+            for (int i = 0; i < s2.Length; i++)
+            {
+                if (s2[i][s2[i].Length - 1] == ch)
+                    s[i] = s[i].Replace(s[i], String.Empty);
+            }
+
+            msg = string.Join(" ", s);
+
+            return msg;
+        }
+
+        public static string Longest(string msg)
+        {
+            msg = DeletePunctuation(msg);
+
+            string[] s = msg.Split(' ');
+            string longest = s[0];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (longest.Length < s[i].Length)
+                    longest = s[i];
+            }
+
+            return longest;
+        }
+
+        public static StringBuilder LongestWords(string msg)
+        {
+            StringBuilder sb;
+            msg = DeletePunctuation(msg);
+            string[] s = msg.Split(' ');
+
+            int average = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                average += s[i].Length;
+            }
+
+            msg = String.Empty;
+            average /= s.Length;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i].Length > average)
+                    msg += $"{String.Join(" ", s[i])} ";
+            }
+            msg.TrimEnd(' ');
+            sb = new StringBuilder(msg);
+
+            return sb;
+        }
+
+        private static string DeletePunctuation(string msg)
+        {
+            StringBuilder sb = new StringBuilder(msg);
+            for (int i = 0; i < sb.Length; i++)
+            {
+                if (char.IsPunctuation(sb[i]))
+                    sb.Remove(i, 1);
+            }
+
+            return sb.ToString();
+        }
+
+        private static string[] DeleteEmptyStrings(string[] msg)
+        {
+            for (int i = 0; i < msg.Length; i++)
+            {
+                if (msg[i] == "")
+                    msg = msg.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+            }
+
+            return msg;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"text.txt";
+            string message = String.Empty;
+            try
+            {
+                message = File.ReadAllText(filePath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            int length = 6;
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine($"Вывести только те слова текста, которые содержат не более {length} букв:");
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine(Message.NoLonger(message, length));
+
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine("Удалить из текста все слова, которые заканчиваются на заданный символ 'я':");
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine(Message.Delete(message, 'я'));
+
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine("Найти самое длинное слово текста:");
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine(Message.Longest(message));
+
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine("Сформировать строку с помощью StringBuilder из самых длинных слов текста:");
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine(Message.LongestWords(message));
+            Console.WriteLine(new string('-', 50));
+
+            Console.WriteLine("Для завершения нажмите любую клавишу.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GeekBrains_HW_5/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+//                                                  Задание 1.
+//  Создать программу, которая будет проверять корректность ввода логина. Корректным логином будет
+//  строка от 2 до 10 символов, содержащая только буквы латинского алфавита или цифры, при этом
+//  цифра не может быть первой:
+//      а) без использования регулярных выражений;
+//      б) с использованием регулярных выражений.
+
+//  Ступин А.А.
+
+namespace GeekBrains_HW_5
+{
+    class Program
+    {
+        static bool IsCorrect(string login)
+        {
+            if (login.Length < 2 || login.Length > 10 || char.IsDigit(login[0]))
+                return false;
+
+            for (int i = 0; i < login.Length; i++)
+            {
+                if (!char.IsDigit(login[i]) && !char.IsLetter(login[i]))
+                    return false;
+                if ((login[i] >= 'А') && (login[i] <= 'Я') ||
+                    (login[i] >= 'а') && (login[i] <= 'я'))
+                    return false;
+            }
+            return true;
+        }
+
+        static void LoginForm1()
+        {
+            string userInput;
+            do
+            {
+                userInput = Console.ReadLine();
+
+                if (IsCorrect(userInput))
+                    Console.WriteLine("Логин корректный.");
+                else
+                    Console.WriteLine("Логин некорректный.");
+            } while (userInput != "quit");
+        }
+
+        static void LoginForm2()
+        {
+            string pattern = @"^[A-Za-z][A-Za-z0-9]{1,9}$";
+            string userInput;
+
+            do
+            {
+                userInput = Console.ReadLine();
+
+                if (Regex.IsMatch(userInput, pattern, RegexOptions.IgnoreCase))
+                    Console.WriteLine("Логин корректный.");
+                else
+                    Console.WriteLine("Логин некорректный.");
+            } while (userInput != "quit");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Корректным логином будет строка от 2 до 10 символов,\n" +
+                              "содержащая только буквы латинского алфавита или цифры,\n" +
+                              "при этом цифра не может быть первой.");
+            Console.WriteLine(new string('-', 54));
+            Console.WriteLine("Выберите режим проверки логина:");
+            Console.WriteLine("1 - Без использования регулярных выражений.");
+            Console.WriteLine("2 - С использованием регулярных выражений.");
+
+            ConsoleKeyInfo input = Console.ReadKey();
+
+            if (input.Key == ConsoleKey.D1 || input.Key == ConsoleKey.D2)
+                Console.WriteLine("\n\nДля выхода введите quit или нажмите Ctrl + C");
+            switch (input.Key)
+            {
+                case ConsoleKey.D1:
+                    Console.WriteLine("\nВведите логин:");
+                    LoginForm1();
+                    break;
+                case ConsoleKey.D2:
+                    Console.WriteLine("\nВведите логин:");
+                    LoginForm2();
+                    break;
+                default:
+                    Console.WriteLine("\n\nНеверный ввод.");
+                    break;
+            }
+            Console.WriteLine("Для завершения нажмите любую клавишу.");
+            Console.ReadKey();
+        }
+    }
+}
